2b35b9bc91f0e008969456a975ea61768078f554
SumanthNarendula <narendulasumanth2@gmail.com>
2025-11-03 10:33:45 +0530

    Wrap App component in StrictMode

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { CartProvider } from "./context/CartContext";
+import React, { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import { CartProvider } from './context/CartContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <CartProvider>
-  <App/>
-</CartProvider>
-)
+  <StrictMode>
+    <CartProvider>
+      <App />
+    </CartProvider>
+  </StrictMode>
+);
